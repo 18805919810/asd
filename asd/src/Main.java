eeaf96418cfd177c1a23ee051aa3fe6521a9367a
To <hello@itcast.cn>
2022-12-08 15:06:44 +0800

    1222

--- asd/src/Main.java
+++ asd/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("1256");
-        System.out.println("1234");
+        System.out.println("1267894");
     }
 
 }
